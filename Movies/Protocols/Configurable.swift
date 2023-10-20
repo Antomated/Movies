58971e2267291749b0c669ced61bf3f27bfb5f70
beavean <beavean@gmail.com>
2023-10-20 12:16:20 +0200

    Add configurable protocol with extension for convenient UI configuration

--- Movies/Protocols/Configurable.swift
+++ Movies/Protocols/Configurable.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+protocol Configurable {}
+
+extension Configurable where Self: AnyObject {
+    @discardableResult
+    func configure(_ action: (Self) throws -> Void) rethrows -> Self {
+        try action(self)
+        return self
+    }
+}
+
+extension NSObject: Configurable {}
