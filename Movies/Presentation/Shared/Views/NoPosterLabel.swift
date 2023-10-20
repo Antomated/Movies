3e6954fd95e501060871126abf7a3415d07dfd83
beavean <beavean@gmail.com>
2023-10-20 14:20:05 +0200

    Add no poster label with localized text

---
+++ Movies/Presentation/Shared/Views/NoPosterLabel.swift
@@ -0,0 +1,32 @@
+//
+//  NoPosterLabel.swift
+//  Movies
+//
+//  Created by Anton Petrov on 22.10.2023.
+//
+
+import UIKit
+
+final class NoPosterLabel: UILabel {
+    init() {
+        super.init(frame: .zero)
+        configureLabel()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureLabel()
+    }
+
+    private func configureLabel() {
+        isHidden = true
+        self.text = LocalizedKey.noPosterLabel.localizedString
+        self.textColor = .white
+        self.backgroundColor = .black
+        self.numberOfLines = 0
+        self.textAlignment = .center
+        self.layer.cornerRadius = Constants.StyleDefaults.cornerRadius
+        self.clipsToBounds = true
+        self.font = .boldSystemFont(ofSize: 20)
+    }
+}
--- Movies/Presentation/Shared/Views/NoPosterPlaceholder.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NoPosterPlaceholder.swift
-//  Movies
-//
-//  Created by Anton Petrov on 22.10.2023.
-//
-
-import Foundation
