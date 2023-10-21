c9a470faf7bc35e6df05f5a90a4181fb9a8f4963
beavean <beavean@gmail.com>
2023-10-21 16:22:15 +0200

    Add view controller extension with close button functionality

--- Movies/Extensions/UIViewController+CloseButton.swift
+++ Movies/Extensions/UIViewController+CloseButton.swift
@@ -5,4 +5,23 @@
 //  Created by Anton Petrov on 22.10.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func setupCloseButton() {
+        guard let navigationController else { return }
+        navigationController.navigationBar.backgroundColor = .systemGroupedBackground
+        let item = UIBarButtonItem(barButtonSystemItem: .close,
+                                   target: self,
+                                   action: #selector(didTapCloseButton))
+        navigationItem.leftBarButtonItem = item
+    }
+
+    @objc private func didTapCloseButton() {
+        if let navigationController, navigationController.presentingViewController != nil {
+            navigationController.dismiss(animated: true)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+}
