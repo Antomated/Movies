7203ae6918e28c777f6f9784b37a799682e508a8
beavean <beavean@gmail.com>
2023-10-21 12:34:11 +0200

    Add UIView and UIViewController extensions with blur applying methods

--- Movies/Extensions/UIView+Blur.swift
+++ Movies/Extensions/UIView+Blur.swift
@@ -5,4 +5,15 @@
 //  Created by Anton Petrov on 21.10.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    func applyBlurEffect(alpha: CGFloat = 0.5) {
+        let blurEffect = UIBlurEffect(style: traitCollection.userInterfaceStyle == .dark ? .dark : .light)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(blurEffectView)
+        self.alpha = alpha
+    }
+}
--- Movies/Extensions/UIViewController+Blur.swift
+++ Movies/Extensions/UIViewController+Blur.swift
@@ -5,4 +5,14 @@
 //  Created by Anton Petrov on 22.10.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func setupBlur() {
+        let blurEffect = UIBlurEffect(style: traitCollection.userInterfaceStyle == .dark ? .dark : .light)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.insertSubview(blurEffectView, at: 0)
+    }
+}
