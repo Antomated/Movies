81589db092fed5edd2bb9e9418e0a607ff7e693c
beavean <beavean@gmail.com>
2023-10-22 16:07:21 +0200

    Update constants

--- Movies/Utilities/Constants.swift
+++ Movies/Utilities/Constants.swift
@@ -9,25 +9,26 @@ import UIKit
 
 enum Constants {
     enum StyleDefaults {
-        static let articleCellHeight: CGFloat = 340
-        static let cornerRadius: CGFloat = 20
+        static let cornerRadius: CGFloat = 16
+        static let mediumPadding: CGFloat = 16
+        static let smallPadding: CGFloat = 8
+        static let pointSeparator: String = " • "
+        static let bigItemSideSize: CGFloat = 56
     }
 
     enum API {
-        static let articlesPerPage = 30
-        static let apiKey = "8b23637d92b147fabc23328863cca344"
-        static let baseUrl = "https://newsapi.org/"
-        static let headLines = baseUrl + "v2/top-headlines"
-        static let sources = headLines + "/sources"
+        static let baseUrl = "https://api.themoviedb.org/3/"
+        static let baseImageUrl = "https://image.tmdb.org/t/p"
+        static let youtubeVideoBaseUrl = "https://www.youtube.com/embed/"
+        static let apiKey = "7ca62b73f595a418f832f4440f9fb21e"
     }
 
     enum SystemImage: String {
-        case bookmarkFill = "bookmark.fill"
-        case bookmark
-        case note
-        case noteText = "note.text"
-        case magnifyingglass
-        case textMagnifyingglass = "text.magnifyingglass"
+        case sortIcon = "arrow.up.arrow.down"
+        case sortAscending = "arrow.up"
+        case sortDescending = "arrow.down"
+        case backButton = "chevron.left"
+        case playButton = "play.circle.fill"
 
         var image: UIImage? { UIImage(systemName: rawValue) }
     }
